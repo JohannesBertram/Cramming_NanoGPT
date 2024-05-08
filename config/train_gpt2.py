a2eca1d14f2324a149ddb8dc89ec1e3d72881eee
JohannesBertram <90758413+JohannesBertram@users.noreply.github.com>
2024-05-08 18:10:04 +0200

    increase batches

--- config/train_gpt2.py
+++ config/train_gpt2.py
@@ -8,13 +8,13 @@ wandb_run_name='gpt2-124M'
 
 # these make the total batch size be ~0.5M
 # 12 batch size * 1024 block size * 5 gradaccum * 8 GPUs = 491,520
-batch_size = 2
-block_size = 16
+batch_size = 12
+block_size = 1024
 gradient_accumulation_steps = 5 * 1
 
 # this makes total number of tokens be 300B
-max_iters = 6000
-lr_decay_iters = 6000
+max_iters = 600000
+lr_decay_iters = 600000
 
 # eval stuff
 eval_interval = 1000
