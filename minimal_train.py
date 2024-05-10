6f7b73c801f77f300c8c7b30a9c3778102a26078
JohannesBertram <90758413+JohannesBertram@users.noreply.github.com>
2024-05-10 15:40:25 +0200

    add debug

--- minimal_train.py
+++ minimal_train.py
@@ -307,6 +307,19 @@ while True:
     # flush the gradients as soon as we can, no need for this memory anymore
     optimizer.zero_grad(set_to_none=True)
 
+    t1 = time.time()
+    dt = t1 - t0
+    t0 = t1
+    if master_process:
+        # get loss as float. note: this is a CPU-GPU sync point
+        # scale up to undo the division above, approximating the true total loss (exact would have been a sum)
+        lossf = loss.item() * gradient_accumulation_steps
+        if local_iter_num >= 5: # let the training loop settle a bit
+            mfu = raw_model.estimate_mfu(batch_size * gradient_accumulation_steps, dt)
+            running_mfu = mfu if running_mfu == -1.0 else 0.9*running_mfu + 0.1*mfu
+        print(f"iter {iter_num}: loss {lossf:.4f}, time {dt*1000:.2f}ms, mfu {running_mfu*100:.2f}%")
+        #train_info[:, iter_num // log_interval] = np.array([iter_num, lossf, running_mfu*100])
+
     iter_num += 1
     local_iter_num += 1
 
