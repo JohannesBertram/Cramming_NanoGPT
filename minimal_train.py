0c4d492c646c4174d0c73af69f3fbe2b291e4f6e
JohannesBertram <90758413+JohannesBertram@users.noreply.github.com>
2024-05-10 15:48:10 +0200

    remove log

--- minimal_train.py
+++ minimal_train.py
@@ -307,7 +307,7 @@ while True:
     # flush the gradients as soon as we can, no need for this memory anymore
     optimizer.zero_grad(set_to_none=True)
 
-    t1 = time.time()
+    """t1 = time.time()
     dt = t1 - t0
     t0 = t1
     if master_process:
@@ -318,7 +318,7 @@ while True:
             mfu = raw_model.estimate_mfu(batch_size * gradient_accumulation_steps, dt)
             running_mfu = mfu if running_mfu == -1.0 else 0.9*running_mfu + 0.1*mfu
         print(f"iter {iter_num}: loss {lossf:.4f}, time {dt*1000:.2f}ms, mfu {running_mfu*100:.2f}%")
-        #train_info[:, iter_num // log_interval] = np.array([iter_num, lossf, running_mfu*100])
+        #train_info[:, iter_num // log_interval] = np.array([iter_num, lossf, running_mfu*100])"""
 
     iter_num += 1
     local_iter_num += 1
