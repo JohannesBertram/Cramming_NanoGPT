26b46cb7fc9919c96ccb74e4177e984d3670d8ea
JohannesBertram <90758413+JohannesBertram@users.noreply.github.com>
2024-05-16 10:59:27 +0200

    log

--- train.py
+++ train.py
@@ -367,7 +367,7 @@ while True:
     optimizer.zero_grad(set_to_none=True)
 
     # timing and logging
-    """t1 = time.time()
+    t1 = time.time()
     dt = t1 - t0
     t0 = t1
     if iter_num % log_interval == 0 and master_process:
@@ -377,7 +377,7 @@ while True:
         if local_iter_num >= 5: # let the training loop settle a bit
             mfu = raw_model.estimate_mfu(batch_size * gradient_accumulation_steps, dt)
             running_mfu = mfu if running_mfu == -1.0 else 0.9*running_mfu + 0.1*mfu
-        print(f"iter {iter_num}: loss {lossf:.4f}, time {dt*1000:.2f}ms, mfu {running_mfu*100:.2f}%")"""
+        print(f"iter {iter_num}: loss {lossf:.4f}, time {dt*1000:.2f}ms, mfu {running_mfu*100:.2f}%")
         
     iter_num += 1
     local_iter_num += 1
