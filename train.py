0f420aa3041e434f936ad67a320d1fd1c176c86a
JohannesBertram <90758413+JohannesBertram@users.noreply.github.com>
2024-05-24 11:35:48 +0200

    mid exp

--- train.py
+++ train.py
@@ -38,8 +38,8 @@ sec_per_day = 79200
 learning_rate = 6e-4 # max learning rate
 
 gradient_accumulation_steps = 8*5*8 # used to simulate larger batch sizes
-min_acc = 1
-max_acc = 1
+min_acc = 32
+max_acc = 32
 acc_increase = 1
 acc_warmup = 0
 use_acc_scheduler = True
@@ -404,7 +404,7 @@ while True:
     # flush the gradients as soon as we can, no need for this memory anymore
     optimizer.zero_grad(set_to_none=True)
 
-    # timing and logging
+    """# timing and logging
     t1 = time.time()
     dt = t1 - t0
     t0 = t1
@@ -415,7 +415,7 @@ while True:
         if local_iter_num >= 5: # let the training loop settle a bit
             mfu = raw_model.estimate_mfu(batch_size * gradient_accumulation_steps, dt)
             running_mfu = mfu if running_mfu == -1.0 else 0.9*running_mfu + 0.1*mfu
-        print(f"iter {iter_num}: loss {lossf:.4f}, time {dt*1000:.2f}ms, mfu {running_mfu*100:.2f}%")
+        print(f"iter {iter_num}: loss {lossf:.4f}, time {dt*1000:.2f}ms, mfu {running_mfu*100:.2f}%")"""
         
     iter_num += 1
     local_iter_num += 1
