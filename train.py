5b9b7a4f86cb9ff658ee3c3ac0efa8fe043f636a
JohannesBertram <90758413+JohannesBertram@users.noreply.github.com>
2024-05-24 16:48:19 +0200

    max

--- train.py
+++ train.py
@@ -38,8 +38,8 @@ sec_per_day = 79200
 learning_rate = 6e-4 # max learning rate
 
 gradient_accumulation_steps = 8*5*8 # used to simulate larger batch sizes
-min_acc = 32
-max_acc = 32
+min_acc = 8*5*8
+max_acc = 8*5*8
 acc_increase = 1
 acc_warmup = 0
 use_acc_scheduler = True
