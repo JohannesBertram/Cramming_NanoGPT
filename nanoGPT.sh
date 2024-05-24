0e7d7affb700f24ccc9c1c485fdc5a8da83d1012
JohannesBertram <90758413+JohannesBertram@users.noreply.github.com>
2024-05-24 13:48:37 +0200

    exp baseline

--- nanoGPT.sh
+++ nanoGPT.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --partition=day
 #SBATCH --gres=gpu:4
-#SBATCH --mem-per-cpu=30
+#SBATCH --mem-per-cpu=150
 #SBATCH --time=1440:00
 #SBATCH --error=job.%J.err
 #SBATCH --output=job.%J.out
