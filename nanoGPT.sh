8f90611cc29bfbd5a18878634ae08547924375cf
JohannesBertram <90758413+JohannesBertram@users.noreply.github.com>
2024-05-27 22:33:30 +0200

    bs 32

--- nanoGPT.sh
+++ nanoGPT.sh
@@ -5,7 +5,7 @@
 #SBATCH --job-name=nanoGPT
 #SBATCH --cpus-per-task=1
 #SBATCH --partition=day
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:4
 #SBATCH --mem-per-cpu=150
 #SBATCH --time=1440:00
 #SBATCH --error=job.%J.err
