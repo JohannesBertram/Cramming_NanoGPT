12b9ab168b59a6aab3b4566d48e70cc98fad80c5
JohannesBertram <90758413+JohannesBertram@users.noreply.github.com>
2024-06-04 23:02:09 +0200

    tokens

--- data/openwebtext/datasettest.py
+++ data/openwebtext/datasettest.py
@@ -7,9 +7,16 @@ train_bin_path = 'ci_train.bin'
 def count_unique_tokens_in_bin(file_path):
     dtype = np.uint16  # Assuming the tokens are stored as uint16
     tokens = np.fromfile(file_path, dtype=dtype)
-    tokens = tokens[:int(np.round(0.5*len(tokens)))]
-    unique_tokens = np.unique(tokens)
-    vocab_size = len(unique_tokens)
+    tokens1 = tokens[:int(np.round(0.25*len(tokens)))]
+    tokens2 = tokens[int(np.round(0.25*len(tokens))):int(np.round(0.5*len(tokens)))]
+    tokens3 = tokens[int(np.round(0.5*len(tokens))):int(np.round(0.75*len(tokens)))]
+    tokens4 = tokens[int(np.round(0.75*len(tokens))):]
+    unique_tokens1 = set(np.unique(tokens1))
+    print("1")
+    unique_tokens2 = set(np.unique(tokens2))
+    unique_tokens3 = set(np.unique(tokens3))
+    unique_tokens4 = set(np.unique(tokens4))
+    vocab_size = len(unique_tokens1.union(unique_tokens2).union(unique_tokens3).union(unique_tokens4))
     return vocab_size
 
 # Get the number of distinct tokens
