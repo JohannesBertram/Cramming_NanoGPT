4331e49704267765082e8a8cec594b36e417210c
JohannesBertram <90758413+JohannesBertram@users.noreply.github.com>
2024-06-11 11:15:44 +0200

    full bs

--- train.py
+++ train.py
@@ -43,12 +43,12 @@ min_lr = 6e-4 # minimum learning rate, should be ~= learning_rate/10 per Chinchi
 
 gradient_accumulation_steps = 8*5*8 # used to simulate larger batch sizes
 min_acc = 32 # min accumuluation steps at start of batch_size schedule
-max_acc = 128
+max_acc = 32
 acc_increase = 1
 acc_warmup = 0
 use_acc_scheduler = True
 
-batch_size = 6 # if gradient_accumulation_steps > 1, this is the micro-batch size
+batch_size = 5 # if gradient_accumulation_steps > 1, this is the micro-batch size
 block_size = 1024
 #mean_batch_size = 300
 #est_sec_per_batch_element = 0.178
