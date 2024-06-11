0056a75c44dadee5a18f6024b2fa9f6373b082ba
JohannesBertram <90758413+JohannesBertram@users.noreply.github.com>
2024-06-11 16:28:48 +0200

    full bs bugfix

--- train.py
+++ train.py
@@ -39,11 +39,11 @@ torch.manual_seed(seed)
 sec_per_day = 79200
 
 learning_rate = 6e-4 # max learning rate
-min_lr = 6e-4 # minimum learning rate, should be ~= learning_rate/10 per Chinchilla
+min_lr = 6e-5 # minimum learning rate, should be ~= learning_rate/10 per Chinchilla
 
 gradient_accumulation_steps = 8*5*8 # used to simulate larger batch sizes
-min_acc = 32 # min accumuluation steps at start of batch_size schedule
-max_acc = 128
+min_acc = 1 # min accumuluation steps at start of batch_size schedule
+max_acc = 32
 acc_increase = 1
 acc_warmup = 0
 use_acc_scheduler = True
